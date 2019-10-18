b2581eadab2499b30509374dcbf1a4b9d64d5614
Valeri Mladenov <valeri.mladenov10@gmail.com>
2019-10-19 01:05:36 +0300

    MyVehcles ready

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./components/Home";
+import MyVehicles from "./components/MyVehicles";
 
 export default function App() {
     return (
         <View>
             <Home />
+            <MyVehicles />
         </View>
     );
 }
