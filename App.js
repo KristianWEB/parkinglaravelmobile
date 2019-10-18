f22afa9c1a62bbd1229b681703ecec16186b4574
Kristian <devWEBpk@outlook.com>
2019-10-19 00:06:27 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./components/Home";
+
+export default function App() {
+    return (
+        <View>
+            <Home />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+});
