04efa94dc2222557d5ab24ef6c6daf3e3717058b
Kristian <devWEBpk@outlook.com>
2019-10-19 16:06:24 +0300

    home implemented

--- components/Home.js
+++ components/Home.js
@@ -1,47 +1,82 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { Button, Header, Avatar, Card } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
-const Home = () => {
+const Home = props => {
     return (
         <View>
             <Header
                 placement="left"
                 leftComponent={{ icon: "menu", color: "#fff" }}
                 centerComponent={{
-                    text: "Natasha Petkova",
-                    style: { color: "#fff" }
+                    text: "Ivan Petrov",
+                    style: { fontWeight: "700", fontSize: 22 }
+                }}
+                containerStyle={{
+                    paddingTop: 25,
+                    backgroundColor: "#fff"
                 }}
             >
                 <Avatar
                     rounded
                     source={{
-                        uri:
-                            "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+                        uri: "https://randomuser.me/api/portraits/men/17.jpg"
                     }}
+                    size={42}
                 />
             </Header>
             <Button
                 buttonStyle={{
                     marginLeft: 25,
                     marginRight: 25,
-                    marginTop: 125,
-                    padding: 75,
+                    marginTop: 50,
+                    paddingTop: 75,
+                    paddingBottom: 75,
+                    paddingLeft: 50,
+                    paddingRight: 50,
                     borderRadius: 15
                 }}
+                titleStyle={{
+                    fontSize: 23,
+                    fontWeight: "700"
+                }}
+                icon={<Icon name="credit-card" size={125} color="white" />}
                 color="#fff"
                 title="Моите превозни средства"
+                onPress={() => props.navigation.navigate("MyVehicles")}
+                linearGradientProps={{
+                    colors: ["#00c6ff", "#0072ff"],
+                    start: { x: 0, y: 0.5 },
+                    end: { x: 1, y: 0.5 }
+                }}
+                iconCenter
             ></Button>
             <Button
                 buttonStyle={{
                     marginLeft: 25,
                     marginRight: 25,
-                    marginTop: 25,
-                    padding: 75,
-                    borderRadius: 15,
-                    backgroundColor: "#512da8"
+                    marginTop: 50,
+                    paddingTop: 75,
+                    paddingBottom: 75,
+                    paddingLeft: 50,
+                    paddingRight: 70,
+                    borderRadius: 15
                 }}
-                title="Плати паркинг"
+                titleStyle={{
+                    fontSize: 23,
+                    fontWeight: "700"
+                }}
+                icon={<Icon name="car-side" size={125} color="white" />}
+                color="#fff"
+                title="Заплати паркинг място"
+                onPress={() => props.navigation.navigate("MyVehicles")}
+                linearGradientProps={{
+                    colors: ["#38ef7d", "#11998e"],
+                    start: { x: 0, y: 0.5 },
+                    end: { x: 1, y: 0.5 }
+                }}
+                iconCenter
             ></Button>
         </View>
     );
