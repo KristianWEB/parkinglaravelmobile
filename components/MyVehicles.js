cde376b7f9c0e24c955778628601f08c5a5d76a4
Kristian <46797006+KristianWEB@users.noreply.github.com>
2019-10-19 16:10:28 +0300

    Vehicles (#3)
    * MyVehcles ready
    
    * Button Add Vehocle

--- components/MyVehicles.js
+++ components/MyVehicles.js
@@ -1,49 +1,92 @@
 import React from "react";
+import { Card, ListItem} from 'react-native-elements'
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {View} from "react-native";
 import { Card, ListItem } from "react-native-elements";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+
 
 const MyVehicles = () => {
     const cars = [
         {
-            name: "ВР 4355 СА",
-            avatar:
-                "https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c46e.png",
-            car_type: "Мотор"
+            name:'ВР 4355 СА',
+            avatar: 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c46e.png',
+            car_type: 'Мотор'
         },
         {
-            name: "ВР 4355 СА",
-            avatar:
-                "https://www.stickpng.com/assets/images/580b585b2edbce24c47b2cf2.png",
-            car_type: "Мотор"
+            name: 'СА 2165 ВН',
+            avatar: 'https://www.stickpng.com/assets/images/580b585b2edbce24c47b2cf2.png',
+            car_type: 'Мотор'
+        },{
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
         },
         {
-            name: "ВР 4355 СА",
-            avatar:
-                "https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png"
-        }
-    ];
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
+        },{
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
+        },{
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
+        },{
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
+        },{
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
+        },{
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
+        },{
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
+        },
+        ,{
+            name: 'Я 6652 ВН',
+            avatar: 'https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png'
+        },
+    ]
     return (
-        <Card containerStyle={{ padding: 0 }}>
-            {cars.map((u, i) => {
-                return (
-                    <ListItem
-                        key={i}
-                        title={u.name}
-                        leftAvatar={{ source: { uri: u.avatar } }}
-                        subtitle={u.subtitle}
-                        bottomDivider
+        <View>
+            <Card containerStyle={{padding: 0}} >
+                {
+                    cars.map((u, i) => {
+                        return (
+                            <ListItem
+                                key={i}
+                                title={u.name}
+                                leftAvatar={{ source: { uri: u.avatar} }}
+                                subtitle={u.subtitle}
+                                bottomDivider
+                            />
+
+                        );
+                    })
+                }
+            </Card>
+            <Button
+                icon={
+                    <Icon
+                        name="plus-circle"
+                        size={20}
+                        color="white"
                     />
-                );
-            })}
-        </Card>
+                }
+                iconleft
+                title=" Добави"
+                titleStyle={{
+                    fontSize: 20
+                }}
+                buttonStyle={{
+                    margin:15,
+                    borderRadius:6,
+                    height:60,
+                }}
+            />
+        </View>
     );
 };
 
-const AppNavigator = createStackNavigator({
-    MyVehicles: {
-        screen: MyVehicles
-    }
-});
-
-export default createAppContainer(AppNavigator);
+export default MyVehicles;
